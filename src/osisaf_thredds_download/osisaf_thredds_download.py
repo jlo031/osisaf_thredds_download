43131d19ebf78bc03041510d87413aa97501b48c
Johannes Lohse <Johannes.Lohse@utas.edu.au>
2025-08-25 11:44:49 +1000

    split helper functions in osi_utils module

--- src/osisaf_thredds_download/osisaf_thredds_download.py
+++ src/osisaf_thredds_download/osisaf_thredds_download.py
@@ -12,54 +12,7 @@ import xarray as xr
 import requests
 from bs4 import BeautifulSoup
 
-# -------------------------------------------------------------------------- #
-# -------------------------------------------------------------------------- #
-
-def check_date(date):
-    try:
-        YYYY, MM, DD = date.split('-')
-    except:
-        logger.error("Input date format must be 'YYYY-MM-DD'. Example: '2023-10-01'")
-        return False
-    return True
-
-
-def check_year_month(year, month):
-    min_year  = 1990
-    max_year  = 2030
-    min_month = 1
-    max_month = 12
-    if not min_year <= int(year) <= max_year:
-        return False
-    if not min_month <= int(month) <= max_month:
-        return False
-    return True
-
-
-def check_hemisphere(hemisphere):
-    if hemisphere not in ['nh', 'sh']:
-        logger.error("Invalid hemisphere. Must be 'nh' (Northern Hemisphere) or 'sh' (Southern Hemisphere).")
-        return False
-    return True
-
-
-def check_download_dir(download_dir):
-    if not download_dir.is_dir():
-        logger.error(f"Could not find download_dir: {download_dir}")
-        return False
-    return True
-
-def check_sensor_daily(sensor):
-    if sensor not in ['amsr2', 'multi']:
-        logger.error("Invalid sensor. Must be 'amsr2' or 'multi'.")
-        return None
-    return True
-
-def check_sensor_level2(sensor):
-    if sensor not in ['amsr2', 'ssmis']:
-        logger.error("Invalid sensor. Must be 'amsr2' or 'ssmis'.")
-        return None
-    return True
+import osisaf_thredds_download.utils as osi_utils
 
 # -------------------------------------------------------------------------- #
 # -------------------------------------------------------------------------- #
@@ -83,10 +36,10 @@ def download_osisaf_daily_SIC(date, hemisphere, sensor, download_dir, overwrite=
 
     # check all inputs
     if not all([
-        check_date(date),
-        check_hemisphere(hemisphere),
-        check_sensor_daily(sensor),
-        check_download_dir(download_dir)
+        osi_utils.check_date(date),
+        osi_utils.check_hemisphere(hemisphere),
+        osi_utils.check_sensor_daily(sensor),
+        osi_utils.check_download_dir(download_dir)
     ]):
         return None
 
@@ -148,10 +101,10 @@ def download_osisaf_full_month_daily_SIC(year, month, hemisphere, sensor, downlo
 
     # check all inputs
     if not all([
-        check_year_month(year, month),
-        check_hemisphere(hemisphere),
-        check_sensor_daily(sensor),
-        check_download_dir(download_dir)
+        osi_utils.check_year_month(year, month),
+        osi_utils.check_hemisphere(hemisphere),
+        osi_utils.check_sensor_daily(sensor),
+        osi_utils.check_download_dir(download_dir)
     ]):
         return None
 
@@ -237,10 +190,10 @@ def download_osisaf_level2_daily_SIC(date, hemisphere, sensor, download_dir, ove
 
     # check all inputs
     if not all([
-        check_date(date),
-        check_hemisphere(hemisphere),
-        check_sensor_daily(sensor),
-        check_download_dir(download_dir)
+        osi_utils.check_date(date),
+        osi_utils.check_hemisphere(hemisphere),
+        osi_utils.check_sensor_daily(sensor),
+        osi_utils.check_download_dir(download_dir)
     ]):
         return None
 
